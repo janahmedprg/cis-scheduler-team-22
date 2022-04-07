8ca816f09cb91a2dc224037122b18f6d204767b5
elgan65536 <elgan256@gmail.com>
2022-04-07 14:55:42 -0400

    added degree interface and updated degreePlan to include a degree

--- src/interfaces/DegreePlan.ts
+++ src/interfaces/DegreePlan.ts
@@ -1,7 +1,9 @@
+import { Degree } from "./Degree";
 import { Semester } from "./Semester";
 
 export interface DegreePlan {
     id: string;
+    degree: Degree;
     name: string;
     semesters: Semester[];
 }

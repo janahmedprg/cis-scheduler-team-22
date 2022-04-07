82040ecd32d4a5eeb7d7d3aa37eb4db0e4353a5c
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-04-07 14:58:11 -0400

    Implemented viewSemester

--- src/Compontents/viewSemester.tsx
+++ src/Compontents/viewSemester.tsx
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import { Semester } from "../interfaces/Semester";
+import { Course } from "../interfaces/Course";
 
-export function MultipleChoice({}: {}): JSX.Element {}
-
+export function MultipleChoice({
+    semester
+}: {
+    semester: Semester;
+}): JSX.Element {
     return (
         <div>
+            {semester.courses.map(
+                (course: Course): JSX.Element => (
+                    <p
+                        key={
+                            semester.id.toString() +
+                            "-" +
+                            course.id.toString() +
+                            "-" +
+                            "course"
+                        }
+                    >
+                        {course}
+                    </p>
+                )
+            )}
         </div>
     );
 }
-

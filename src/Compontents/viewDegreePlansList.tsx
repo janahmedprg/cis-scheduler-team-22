20ba32e841c20d370d564ace6263be3581759ab6
Brandon Aguiar <brandonaguiar@wifi-roaming-128-4-146-50.wifi.udel.edu>
2022-04-10 00:50:33 -0400

    DegreePlansList update

--- src/Compontents/viewDegreePlansList.tsx
+++ src/Compontents/viewDegreePlansList.tsx
@@ -3,12 +3,25 @@ import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import { DegreePlan } from "../interfaces/DegreePlan";
 
-export function DegreePlans({}: {}): JSX.Element {}
-    const [degreePlans, setDegreePlans] = useState<DegreePlan[]>();
+export function DegreePlans({
+    degreePlan
+}: {
+    degreePlan: DegreePlan;
+}): JSX.Element {
+    //keep the array here?
+    let degreePlansList: DegreePlan[] = [];
 
     return (
         <div>
+            <div>
+                {degreePlansList.map(
+                    (degreePlanOption: DegreePlan): JSX.Element => (
+                        <p key={degreePlanOption.id.toString()}>
+                            {degreePlanOption.name}
+                        </p>
+                    )
+                )}
+            </div>
         </div>
     );
 }
-

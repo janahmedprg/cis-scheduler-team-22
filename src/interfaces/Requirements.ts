54209852bb85910cb7c4e3dd569924f96fd1c608
elgan65536 <elgan256@gmail.com>
2022-04-23 11:17:14 -0400

    added Requirements interface

---
+++ src/interfaces/Requirements.ts
@@ -0,0 +1,16 @@
+export interface Requirements {
+    /* each field represents the number of credits in that requirement */
+    /* breadth fields are abbreviations for 
+    Creative Arts and Humanities;
+    History and Cultural Change;
+    and Social and Behavioral Science
+    respectively. */
+    CAHBreadth: number;
+    HCCBreadth: number;
+    SBSBreadth: number;
+    ForeignLanguage: number;
+    LabScience: number;
+    CSCore: number;
+    TechnicalElective: number;
+    CSCapstone: number;
+}

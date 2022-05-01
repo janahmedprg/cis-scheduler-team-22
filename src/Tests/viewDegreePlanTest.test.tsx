ef020bb23d1a74354566b3e0dadf65d279332338
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-05-01 11:41:44 -0400

    more tests

--- src/Tests/viewDegreePlanTest.test.tsx
+++ src/Tests/viewDegreePlanTest.test.tsx
@@ -27,4 +27,49 @@ describe("EditQuizView Tests", () => {
             screen.getByTestId("7-remove-semester-degree-plan")
         ).toBeInTheDocument();
     });
+    test("Test Edit this Degree Plan button", () => {
+        expect(screen.queryAllByText("Editing: Math test plan")).toHaveLength(
+            0
+        );
+        expect(screen.queryAllByText("New degree plan Name:")).toHaveLength(0);
+        screen.getByTestId("7-edit-close-degree-plan").click();
+        expect(screen.getAllByText("Editing: Math test plan")).toHaveLength(1);
+        expect(screen.getAllByText("New degree plan Name:")).toHaveLength(1);
+        screen.getByTestId("7-edit-close-degree-plan").click();
+        expect(screen.queryAllByText("Editing: Math test plan")).toHaveLength(
+            0
+        );
+        expect(screen.queryAllByText("New degree plan Name:")).toHaveLength(0);
+    });
+    test("Test Add Courses To Plan button", () => {
+        expect(
+            screen.queryByTestId("7-close-add-course-degree-plan")
+        ).toBeFalsy();
+        expect(screen.queryAllByText("Course Adder")).toHaveLength(0);
+        expect(
+            screen.queryAllByText("Enter course Code: (Ex: CISC)")
+        ).toHaveLength(1);
+        expect(
+            screen.queryAllByText("Enter course Number: (Ex: 108)")
+        ).toHaveLength(1);
+        screen.getByTestId("7-add-course-degree-plan").click();
+        expect(screen.getAllByText("Course Adder")).toHaveLength(1);
+        expect(
+            screen.getAllByText("Enter course Code: (Ex: CISC)")
+        ).toHaveLength(2);
+        expect(
+            screen.getAllByText("Enter course Number: (Ex: 108)")
+        ).toHaveLength(2);
+        expect(
+            screen.getByTestId("7-close-add-course-degree-plan")
+        ).toBeInTheDocument();
+        screen.getByTestId("7-close-add-course-degree-plan").click();
+        expect(screen.queryAllByText("Course Adder")).toHaveLength(0);
+        expect(
+            screen.queryAllByText("Enter course Code: (Ex: CISC)")
+        ).toHaveLength(1);
+        expect(
+            screen.queryAllByText("Enter course Number: (Ex: 108)")
+        ).toHaveLength(1);
+    });
 });
